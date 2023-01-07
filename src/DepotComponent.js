5c5ca925cfbaf332e4629f9d02f3aaa9166e2691
Marc <marc.hoeper@gmx.de>
2023-01-07 18:53:04 +0000

    Add title to depot

--- src/DepotComponent.js
+++ src/DepotComponent.js
@@ -10,30 +10,32 @@ export default class DepotComponent extends SimpleComponent {
   /* Prevent drag from https://stackoverflow.com/questions/63758425/react-ondragstart-doesnt-fire-in-material-ui-autocomplete-component */
   render() {
     return (
-      <div
-        className="depot-container"
-        class="depot-container"
-        onClick={(evt) => console.log("click")}
-        onMouseDown={(event) => event.stopPropagation()}
-      >
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Kaufpreis</th>
-              <th>Aktueller Preis</th>
-              <th>Wert</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>MSCI World</td>
-              <td>76,40€</td>
-              <td>80,00€</td>
-              <td>13.120,42€</td>
-            </tr>
-          </tbody>
-        </table>
+      <div className="depot-container">
+        <div>Depot</div>
+        <div
+          class="depot-container"
+          onClick={(evt) => console.log("click")}
+          onMouseDown={(event) => event.stopPropagation()}
+        >
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Kaufpreis</th>
+                <th>Aktueller Preis</th>
+                <th>Wert</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>MSCI World</td>
+                <td>76,40€</td>
+                <td>80,00€</td>
+                <td>13.120,42€</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     );
   }
