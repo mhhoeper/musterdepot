2a514e1aff1cf647309b47c2ce42531af6267b2d
Marc Hoeper <marc.hoeper@gmx.de>
2023-01-17 00:37:00 +0100

    Test if the correct pages are built

--- src/MyResponsiveGrid.tsx
+++ src/MyResponsiveGrid.tsx
@@ -9,7 +9,8 @@ function getLayoutsFromSomewhere() {
     const layout = [
         { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
         { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-        { i: "c", x: 4, y: 0, w: 1, h: 2 }
+        { i: "c", x: 4, y: 0, w: 1, h: 2 },
+        { i: "d", x: 0, y: 2, w: 1, h: 2 }
     ];
     return {layout};
 }
