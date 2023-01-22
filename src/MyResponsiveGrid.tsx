71f71b2f20bf2101af46e986cd636913df3868b0
Marc Hoeper <marc.hoeper@gmx.de>
2023-01-22 20:43:20 +0100

    finish persistance

--- src/MyResponsiveGrid.tsx
+++ src/MyResponsiveGrid.tsx
@@ -1,38 +1,19 @@
 import React from "react";
 import ReactGridLayout, { Responsive, WidthProvider } from "react-grid-layout";
+import { getFromLS, safeToLS } from "./LocalStorage";
 import DepotComponent from "./DepotComponent";
 import DepotComponent2 from "./DepotComponent2";
 import "./Grid.css"
 
 
-function getFromLS(key: string): any {
-    let ls: any = {};
-    console.log("getFromLS");
-    if (global.localStorage) {
-        try {
-            ls = JSON.parse(global.localStorage.getItem("musterdepot-storage") || "");
-            console.log("This is what we got: " + global.localStorage.getItem("musterdepot-storage"));
-        }
-        catch (e) {
-            // Ignore
-        }
-    }
-    return ls[key];
-}
-
-function safeToLS(key: string, value: any) {
-    if (global.localStorage) {
-        global.localStorage.setItem(
-            "musterdepot-storage",
-            JSON.stringify({
-                [key]: value
-            })
-        );
-    }
-}
-
-
-const originalLayouts = getFromLS("layouts") || {};
+const layoutlg = [
+    { i: "a", x: 4, y: 6, w: 2, h: 2},
+    { i: "b", x: 0, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
+    { i: "c", x: 0, y: 3, w: 3, h: 2 },
+    { i: "d", x: 6, y: 6, w: 2, h: 2, static: true  }
+];
+const presetlayouts = {lg: layoutlg};
+const originalLayouts = getFromLS("layouts") || presetlayouts;
 
 const ResponsiveGridLayout = WidthProvider(Responsive);
 
@@ -53,13 +34,6 @@ class MyResponsiveGrid extends React.Component<{}, {layouts: ReactGridLayout.Lay
     }
 
     render() {
-        const layoutlg = [
-            { i: "a", x: 4, y: 6, w: 2, h: 2},
-            { i: "b", x: 0, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-            { i: "c", x: 0, y: 3, w: 3, h: 2 },
-            { i: "d", x: 6, y: 6, w: 2, h: 2, static: true  }
-        ];
-        const presetlayouts = {lg: layoutlg};
         console.log("Compare local storage layout in state variable and old preset:\n" + JSON.stringify(this.state.layouts) + "\n" + JSON.stringify(presetlayouts));
         return (
             <ResponsiveGridLayout
