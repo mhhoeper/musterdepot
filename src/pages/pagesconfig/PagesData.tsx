1c03badff9cdca83736850f7848a081b2df1471d
Marc Hoeper <marc.hoeper@gmx.de>
2023-02-11 23:36:13 +0100

    Changed from BrowserRouter to HashRouter and centralized page configuration

---
+++ src/pages/pagesconfig/PagesData.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import * as FaIcons from 'react-icons/fa'  // see https://react-icons.github.io/react-icons/icons?name=fa
+
+// The different pages
+import MyResponsiveGrid from "../../MyResponsiveGrid";
+import MobileDepot from "../../pages/MobileDepot";
+import SettingsPanel from "../../SettingsPanel";
+
+export const PagesData = [
+    {
+        path: '/',
+        title: 'Desktop Ansicht',
+        icon: <FaIcons.FaDesktop />,
+        content: <MyResponsiveGrid />
+    },
+    {
+        path: '/mobildepot',
+        title: 'Mobile Depot',
+        icon: <FaIcons.FaDollarSign />,
+        content: <MobileDepot />
+    },
+    {
+        path: '/settings',
+        title: 'Settings',
+        icon: <FaIcons.FaRegSun />,
+        content: <SettingsPanel />
+    }
+]
